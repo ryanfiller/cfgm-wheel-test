de1a1bc85758e38768ce52a99733bb1f90f22168
Ryan Filler <ryanfiller89@gmail.com>
2019-02-06 20:39:53 -0600

    fixes loops

--- src/components/wheel.js
+++ src/components/wheel.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import { paths } from './paths'
+import { dates } from '../data'
 
 const Group = (props) => { 
 
-    const {start, end} = props
+    const {
+        start,
+        end, 
+        color
+    } = props
+
+    console.log(props)
 
     const arcs = [];
-
-    for (let i = start; i <= end; i++) {
-        arcs.push(paths[i])
+    
+    for (let i = start; i < end; i++) {
+        arcs.push(React.cloneElement(paths[i], {key: i, style: {stroke: color}}))
     }
 
-    console.log(arcs)
+    // deal with rollover in loop
+    if (end < start) {
+        for (let i = start; i < dates.length; i++) {
+            arcs.push(React.cloneElement(paths[i], {key: i, style: {stroke: color}}))
+        }
+
+        if (end > 0) {
+            for (let i = 0; i < end; i++) {
+                arcs.push(React.cloneElement(paths[i], {key: i, style: {stroke: color}}))
+            }
+        }
+    }
 
     return (
         <g className={'test'}>
@@ -29,7 +47,12 @@ const Wheel = (props) => {
             <svg version="1.1" id="wheel" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 500 500" >
                 {steps.map( (step, index) => {
                     return (
-                        <Group {...step} key={index} />
+                        <Group 
+                            start={step.start} 
+                            end={index + 1 < steps.length ? steps[index + 1].start : steps[0].start}
+                            color={step.color}
+                            key={index} 
+                        />
                     )
                 })}
             </svg>
--- src/data.js
+++ src/data.js
@@ -1,23 +1,47 @@
+export const dates = [
+    'Early January',
+    'Mid January',
+    'Early February',
+    'Mid February',
+    'Early March',
+    'Mid March',
+    'Early April',
+    'Mid April',
+    'Early May',
+    'Mid  May',
+    'Early June',
+    'Mid June',
+    'Early July',
+    'Mid July',
+    'Early August',
+    'Mid August',
+    'Early September',
+    'Mid September',
+    'Early October',
+    'Mid October',
+    'Early Novemeber',
+    'Mid Novemeber',
+    'Early December',
+    'Mid December'
+]
+
 export const steps = [
     {
         title: 'Step 1',
         text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur.',
-        start: 0,
-        end: 5,
-        color: 'orangered'
+        start: 1,
+        color: 'blue'
     },
     {
         title: 'Step 2',
         text: 'Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus.',
         start: 5,
-        end: 6,
         color: 'slategray'
     },
     {
         title: 'Step 3',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         start: 6,
-        end: 7,
         color: 'lightblue'
     }
 ]
