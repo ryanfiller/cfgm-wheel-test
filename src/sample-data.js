f3f61994d86f0ddca7c6303bfb6c3c2bb495a0da
Ryan Filler <ryanfiller89@gmail.com>
2019-02-08 00:33:14 -0600

    swaps colors

--- src/sample-data.js
+++ src/sample-data.js
@@ -15,7 +15,7 @@ export const steps = [
         title: 'Step 3',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         start: 7,
-        color: 'lightblue'
+        color: 'aqua'
     },
     {
         title: 'Step between 3 and 4',
@@ -27,12 +27,12 @@ export const steps = [
         title: 'Step 4',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         start: 15,
-        color: 'green'
+        color: 'lawngreen'
     },
     {
         title: 'Step 5',
         text: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
         start: 19,
-        color: 'red'
+        color: 'orangered'
     }
 ]
