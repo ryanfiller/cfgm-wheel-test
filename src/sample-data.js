8665ee0dc4506f0dd686e38799bce45ce225e0fa
Ryan Filler <ryanfiller89@gmail.com>
2019-02-08 00:27:59 -0600

    more steps

--- src/sample-data.js
+++ src/sample-data.js
@@ -2,7 +2,7 @@ export const steps = [
     {
         title: 'Step 1',
         text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur.',
-        start: 0,
+        start: 1,
         color: 'lawngreen'
     },
     {
@@ -24,13 +24,19 @@ export const steps = [
         color: 'purple'
     },
     {
-        title: 'Step 4',
+        title: 'Step 5',
+        text: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+        start: 13,
+        color: 'orangered'
+    },
+    {
+        title: 'Step 6',
         text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
         start: 15,
         color: 'blue'
     },
     {
-        title: 'Step 5',
+        title: 'Step 7',
         text: 'Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit.',
         start: 19,
         color: 'maroon'
