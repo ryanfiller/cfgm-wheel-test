4153b788c5f72369317b24f31c5a764f887afac7
Ryan Filler <ryanfiller89@gmail.com>
2019-02-07 21:15:01 -0600

    corrects mid point in format-data

--- src/helpers/format-data.js
+++ src/helpers/format-data.js
@@ -1,3 +1,5 @@
+import { paths } from '../components/paths'
+
 const formatData = (data) => {
 
     const result = [];
@@ -6,7 +8,9 @@ const formatData = (data) => {
         return (
             result[index] = item,
             result[index].end = index + 1 < data.length ? data[index + 1].start : data[0].start,
-            result[index].span = result[index].end - result[index].start,
+            result[index].span = (result[index].end - result[index].start) > 0 ? 
+                result[index].end - result[index].start : 
+                (result[index].end - result[index].start) + paths.length,
             result[index].middle = result[index].end - (result[index].span / 2)
         );
     })
--- src/sample-data.js
+++ src/sample-data.js
@@ -2,13 +2,13 @@ export const steps = [
     {
         title: 'Step 1',
         text: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur.',
-        start: 0,
+        start: 1,
         color: 'blue'
     },
     {
         title: 'Step 2',
         text: 'Aenean lacinia bibendum nulla sed consectetur. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec id elit non mi porta gravida at eget metus.',
-        start: 3,
+        start: 4,
         color: 'slategray'
     },
     {
